28d5c6efa1fa91e2a2cb94002d0d743a1f47066c
ayushkhaire <codeeayush@gmail.com>
2024-10-14 00:52:23 +0530

    handled url for data rendering stock for daily update.

--- stocksly/scrapper/urls.py
+++ stocksly/scrapper/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('get_available_stocks/', views.get_available_stocks),
+    path('get_stock_daily_data/<str:stocksymbol>/', views.get_stocks_daily_data),
 ]
