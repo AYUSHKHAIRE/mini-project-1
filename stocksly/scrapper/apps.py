e97057984a360dabfa315635922c1bc05542df27
ayushkhaire <codeeayush@gmail.com>
2024-10-26 12:49:54 +0530

    apps.py

--- stocksly/scrapper/apps.py
+++ stocksly/scrapper/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class ScrapperConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scrapper'
+
+    def ready(self):
+        from .signals import schedule_tasks
+        post_migrate.connect(schedule_tasks, sender=self)
